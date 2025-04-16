2100e864e73f9d4bc6e7075e79824e0d35544609
sravanthi-25 <sravanthipalvai2025@gmail.com>
2025-04-16 20:43:21 +0530

    Update Billing&Shipping Address

--- IndiaLivings_Web_UI/Models/UserAddressViewModel.cs
+++ IndiaLivings_Web_UI/Models/UserAddressViewModel.cs
@@ -6,28 +6,31 @@ namespace IndiaLivings_Web_UI.Models
 {
     public class UserAddressViewModel
     {
-        public int UserAddressID { get; set; }
-        public int UserID { get; set; }
-        public string UserContactFullAddress { get; set; }
-        public string UserContactCity { get; set; }
-        public string UserContactState { get; set; }
-        public string UserContactCountry { get; set; }
-        public string UserContactPinCode { get; set; }
-        public string UserBillingFullAddress { get; set; }
-        public string UserBillingCity { get; set; }
-        public string UserBillingState { get; set; }
-        public string UserBillingCountry { get; set; }
-        public string UserBillingPinCode { get; set; }
-        public string UserShippingFullAddress { get; set; }
-        public string UserShippingCity { get; set; }
-        public string UserShippingState { get; set; }
-        public string UserShippingCountry { get; set; }
-        public string UserShippingPinCode { get; set; }
-        public bool IsActive { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public int CreatedBy { get; set; }
-        public DateTime UpdatedDate { get; set; }
-        public int UpdatedBy { get; set; } 
+        
+        public int intUserID { get; set; }
+        public string strUserContactFullAddress { get; set; }
+        public string strUserContactCity { get; set; }
+        public string strUserContactState { get; set; }
+        public string strUserContactCountry { get; set; }
+        public string strUserContactPinCode { get; set; }
+        public int intUserAddressType { get; set; }
+
+        public string UpdateAddress(int intUserID, string strUserContactFullAddress, string strUserContactCity, string strUserContactState, string strUserContactCountry, string strUserContactPinCode, int intUserAddressType)
+        {
+            AuthenticationHelper PH = new AuthenticationHelper();
+            var response = "";
+            try
+            {
+                response = PH.UpdateAddress(intUserID, strUserContactFullAddress, strUserContactCity, strUserContactState, strUserContactCountry, strUserContactPinCode, intUserAddressType);
+            }
+            catch (Exception ex)
+            {
+                ErrorLog.insertErrorLog(ex.Message, ex.StackTrace, ex.Source);
+            }
+            return response;
+        }
+
+
 
     }
 }
