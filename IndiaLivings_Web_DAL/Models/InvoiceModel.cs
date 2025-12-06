84f092a6a3052b392f35aba4a787d647abec0909
sarada123 <vajjha.sravani@gmail.com>
2025-12-06 23:00:11 +0530

    Changes to Invoice

--- IndiaLivings_Web_DAL/Models/InvoiceModel.cs
+++ IndiaLivings_Web_DAL/Models/InvoiceModel.cs
@@ -30,12 +30,12 @@ namespace IndiaLivings_Web_DAL.Models
         public string? invoiceNumber { get; set; } = string.Empty;
         public decimal invoiceTotal { get; set; }
         public decimal taxAmount { get; set; }
-        public InvoiceStatus Status { get; set; } = InvoiceStatus.PENDING;
+        public InvoiceStatus Status { get; set; }// = InvoiceStatus.PENDING;
         public decimal discountAmount { get; set; }
-        public DateTime createdDate { get; set; } = DateTime.MinValue;
-        public DateTime dueDate { get; set; } = DateTime.MinValue;
+        public DateTime createdDate { get; set; }// = DateTime.MinValue;
+        public DateTime dueDate { get; set; }//= DateTime.MinValue;
          public string description { get; set; } = string.Empty;
-         public DateTime updatedDate { get; set; } = DateTime.MinValue;
+        public DateTime updatedDate { get; set; }//= DateTime.MinValue;
          public string updatedBy { get; set; } = string.Empty;
          public string invoiceItemName { get; set; } = string.Empty;
          public string invoiceItemDescription { get; set; } = string.Empty;
